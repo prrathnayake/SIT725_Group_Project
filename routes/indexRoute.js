760f3a409d41c02538c3302c88c135afb5cc1f5f
prrathnayake <pasanransika1@gmail.com>
2024-04-15 22:23:05 +1000

    update route files

---
+++ routes/indexRoute.js
@@ -0,0 +1,10 @@
+let express = require("express");
+let router = express.Router();
+
+router.get("/", async function (req, res) {
+});
+
+router.post("/", function (req, res) {
+});
+
+module.exports = router;
---
+++ routes/loginRoute.js
@@ -0,0 +1,11 @@
+let express = require("express");
+let router = express.Router();
+let loginController = require("../controller/loginController.js");
+
+router.get("/", async function (req, res) {
+});
+
+router.post("/", function (req, res) {
+});
+
+module.exports = router;
