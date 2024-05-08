c3761dab1a6ca41f8837bb5ebf3d69e33fe7ab70
tharinduDeakin <s224169428@deakin.edu.au>
2024-05-08 22:33:13 +1000

    incorparating message broker

--- controller/employeeController.js
+++ controller/employeeController.js
@@ -1,23 +1,50 @@
 const EmployeeService = require("../model/service/employeeservice");
 const Employee = require("../model/employeeModel");
-
+const {
+  sendMessageToQueue,
+} = require("../middleware/websocket-server/rabbitmqProducer");
 
 const updateEmployee = async (req, res) => {
-    const { empId, firstName, lastName, dateOfBirth, address, phone, designation, status, department } = req.body;
+  const {
+    empId,
+    firstName,
+    lastName,
+    dateOfBirth,
+    address,
+    phone,
+    designation,
+    status,
+    department,
+  } = req.body;
 
-    try {
-        const upsertedId = await EmployeeService.updateEmployee(
-            new Employee(empId, firstName, lastName, dateOfBirth, address, phone, designation, department, status)
-        );
+  try {
+    const upsertedId = await EmployeeService.updateEmployee(
+      new Employee(
+        empId,
+        firstName,
+        lastName,
+        dateOfBirth,
+        address,
+        phone,
+        designation,
+        department,
+        status
+      )
+    );
 
-        if (upsertedId) {
-            return res.status(200).json({ statusCode: 200, message: "Employee details updated successfully" });
-        } else {
-            return res.status(500).json({ statusCode: 500, message: "Failed to update employee" });
-        }
-    } catch (error) {
-        console.log(error);
+    if (upsertedId) {
+      return res.status(200).json({
+        statusCode: 200,
+        message: "Employee details updated successfully",
+      });
+    } else {
+      return res
+        .status(500)
+        .json({ statusCode: 500, message: "Failed to update employee" });
     }
+  } catch (error) {
+    console.log(error);
+  }
 };
 
 const addEmployee = async (req, res) => {
